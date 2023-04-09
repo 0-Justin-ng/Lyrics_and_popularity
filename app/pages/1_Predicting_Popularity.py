93247494416765e130f3f6d5a23e592ab5f0bbc3
0-justin-ng <0.justin.ng@gmail.com>
2023-04-09 04:11:03 -0400

    Fixed error with accessing parent directory.

--- app/pages/1_Predicting_Popularity.py
+++ app/pages/1_Predicting_Popularity.py
@@ -5,7 +5,8 @@ import sys
 from pathlib import Path
 
 
-sys.path.append(str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent.parent.parent))
+
 
 from utilities import utils
 import joblib
